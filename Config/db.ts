66a8f2fde18155bbc3112a2b95c3653a4bf46625
NithusanK <nithusank@hotmail.com>
2022-04-02 23:31:49 -0400

    added Live Site for section 3

---
+++ Config/db.ts
@@ -0,0 +1,4 @@
+export const LocalURI = "mongodb://localhost/contacts";
+export const RemoteURI="mongodb+srv://nithusank:HalyoAf2mH3k8l5h@cluster0.kim5y.mongodb.net/contacts?retryWrites=true&w=majority"
+export const SessionSecret = "WEBD6201SessionSecret";
+export const HostName = "Mongo DB Atlas";
